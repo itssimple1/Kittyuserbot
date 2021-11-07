1f455ccb722954de2ac05acd2e0cd73cbd1566f1
itssimple1 <91212696+itssimple1@users.noreply.github.com>
2021-11-08 02:44:17 +0530

    Create string.sh

---
+++ string.sh
@@ -0,0 +1,13 @@
+echo """
+                      © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_KITTY™
+                           KITTY
+                           KITTY
+                           KITTY
+                           KITTY
+           
+           
+                     © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_KITTY™                       
+                         @kittyBot_Support
+"""
+
+python3 string_session.py
